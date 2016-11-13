603158dbb1ef90ab6ca0b8945fe2b416b8e44ed7
nbaruah <nayanmanibaruah@gmail.com>
2016-11-14 00:39:13 +0530

    Graph processing with DFS to answer various path related questions

---
+++ src/main/java/graph/undirected/Paths.java
@@ -0,0 +1,80 @@
+package graph.undirected;
+
+import java.util.Stack;
+
+/**
+ * This class answers various path related questions from a source vertex s.
+ * It is initialized With a graph G and a source vertex s.
+ * On initialization it performs a DFS from the source vertex and computes various path related questions.
+ * Created by nbaruah on 11/13/2016.
+ */
+public class Paths {
+    private boolean[] visited; // vertex indexed array to mark when a vertex is visited
+    private int[] reachedFrom; // vertex indexed array to track back from which vertex we reached a particular vertex for the first time
+    private int source; // Stores the source vertex
+    private int sizeOfG; // Stores the number of vertices in the graph to be processed
+
+    /**
+     * Find paths in G from source
+     * @param G the Graph
+     * @param source vertex
+     */
+    public Paths(Graph G, int source){
+        sizeOfG = G.V();
+        validateVertex(source);
+        this.source = source;
+        visited = new boolean[G.V()];
+        reachedFrom = new int[G.V()];
+        dfs(G, source);
+    }
+
+    public boolean hasPathTo(int v){
+        validateVertex(v);
+        return visited[v];
+    }
+
+    public Iterable<Integer> pathTo(int v){
+        if (!hasPathTo(v)){
+            return null;
+        }
+        Stack<Integer> path = new Stack<Integer>();
+        int currentVertex = v;
+        while (currentVertex != this.source){
+            path.push(currentVertex);
+            currentVertex = reachedFrom[currentVertex];
+        }
+        path.push(this.source);
+        return path;
+    }
+
+    private void dfs(Graph G, int source){
+        visited[source] = true;
+        for (int adjV : G.getAdjVertices(source)){
+            if (!visited[adjV]){
+                dfs(G, adjV);
+                reachedFrom[adjV] = source;
+            }
+        }
+    }
+
+    private void validateVertex(int v){
+        if (v < 0 || v >= sizeOfG){
+            throw new ArrayIndexOutOfBoundsException("Vertex: " + v + " is not in between 0 and " + (sizeOfG - 1));
+        }
+    }
+
+    public static void main(String[] args){
+        Graph G = new AdjacencyList(6);
+        G.addEdge(0, 1);
+        G.addEdge(0, 2);
+        G.addEdge(1, 3);
+        G.addEdge(3, 4);
+
+        Paths pathsFrom_0 = new Paths(G, 0);
+        for (int v : pathsFrom_0.pathTo(2)){
+            System.out.print("=> " +v);
+        }
+
+        pathsFrom_0.hasPathTo(7);
+    }
+}
